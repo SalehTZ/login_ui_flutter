a742ab373483ff15f01f0122906c4c534f427347
salehtz <m.s_ta@live.com>
2021-12-17 21:00:29 +0330

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         accentColor: accentColorC,
         primaryColor: primaryColorC,
