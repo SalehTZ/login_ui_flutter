d6e50d19fe14e10c0d81d45be423ad2cb97518b8
salehtz <m.s_ta@live.com>
2021-12-17 21:01:25 +0330

    added an id attribute to button

--- lib/components/round_btn.dart
+++ lib/components/round_btn.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:login_ui/constants/theme_data.dart';
 import 'package:login_ui/screens/main_screen.dart';
 import 'package:login_ui/constants/globals.dart' as globals;
+import 'package:login_ui/components/dialogs/sign_up_success.dart';
 
 class CustomButton extends StatelessWidget {
+  final int _iD;
   final String _btnText;
   // final String _inputUsername;
   // final String _inputPassword;
-  const CustomButton(this._btnText, {Key key}) : super(key: key);
+  const CustomButton(this._iD, this._btnText, {Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -24,20 +26,29 @@ class CustomButton extends StatelessWidget {
           style: TextStyle(fontSize: 16, color: accentColorC),
         ),
         onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) =>
-                  MainScreen(globals.inputUsername, globals.inputPassword),
-            ),
-          );
+          if (_iD == 1)
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) {
+                  return MainScreen(globals.inputEmail, globals.inputPassword);
+                },
+              ),
+            );
+          else
+            Navigator.push(
+              context,
+              PageRouteBuilder(
+                pageBuilder: (context, _, __) => AlertSignIn(
+                  'Successful',
+                  'Sign Up was successful',
+                  'Ok',
+                ),
+                opaque: false,
+              ),
+            );
         },
       ),
     );
   }
 }
-
-// Widget _loginRoundBtn(String _btnText, String _inputUsername,
-//     String _inputPassword, BuildContext context) {
-//   return
-// }
